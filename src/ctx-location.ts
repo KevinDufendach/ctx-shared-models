95c1c65c803a5dc55a958a2467d0da4bab3537a9
Kevin R. Dufendach, MD, MS <kevin.dufendach@cchmc.org>
2024-04-22 11:55:22 -0400

    make operational status and occupant optional

--- src/ctx-location.ts
+++ src/ctx-location.ts
@@ -7,11 +7,11 @@ export interface CtxLocation extends CtxResource {
     identifier: string
     physicalType: 'si' | 'bu' | 'wa' | 'ro' | 'bd' | 'ho'
 
-    operationalStatus: 'occupied' | 'unoccupied' | 'leaving' | 'closed' | 'housekeeping' | 'reserved' | 'contaminated' | 'isolated'
+    operationalStatus?: 'occupied' | 'unoccupied' | 'leaving' | 'closed' | 'housekeeping' | 'reserved' | 'contaminated' | 'isolated'
     operationalStatusText?: string
 
-    occupant: string | null
-    futureOccupants: string[]
+    occupant?: string | null
+    futureOccupants?: string[]
 
     partOf?: string // ID of another location this is physically a part of
     characteristics?: CodeableConcept[]
