fd574e85eeda95e789a01e76c0b7e3cb23a18ad4
Kevin R. Dufendach, MD, MS <kevin.dufendach@cchmc.org>
2024-04-28 19:37:16 -0400

    add generic type to CtxListEntry and CtxList

--- src/ctx-list.ts
+++ src/ctx-list.ts
@@ -1,17 +1,18 @@
 import {CtxResource} from "./ctx-resource";
 import {CodeableConcept} from "fhir/r4";
 
-export interface CtxListEntry { // C? Entries in the list
+export interface CtxListEntry<T> { // C? Entries in the list
+  entryId: string,
   flag?: CodeableConcept[], // Status/Workflow information about this item
   date: Date, // When item added to list
   itemId: string // R!  Actual entry
   deleted?: boolean, // C? If this item is actually marked as deleted,
-  data?: any
+  data?: T
 }
 
-export interface CtxList extends CtxResource {
+export interface CtxList<T> extends CtxResource {
   resourceType: "CtxList",
   identifier: string, // Business identifier
   title: string, // Descriptive name for the list
-  entries: CtxListEntry[],
+  entries: CtxListEntry<T>[],
 }
