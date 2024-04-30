f52b326ce45eb1368b241c7e0a9bad694e63fc48
Kevin R. Dufendach, MD, MS <kevin.dufendach@cchmc.org>
2024-04-30 12:36:12 -0400

    fix script not updating correct shared models

--- update.ps1
+++ update.ps1
@@ -8,13 +8,13 @@ git push
 cd ../caretrax/frontend
 Write-Host "Updating frontend"
 pwd
-yarn upgrade wb-shared-models
+yarn upgrade ctx-shared-models
 Write-Host "Frontend updated successfully"
 
 cd ../api
 Write-Host "Updating API"
 pwd
-yarn upgrade wb-shared-models
+yarn upgrade ctx-shared-models
 Write-Host "API updated successfully"
 
 cd ../../ctx-shared-models
