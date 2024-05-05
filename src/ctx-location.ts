fa9efd735e0605e2ca4a1ae36adc8d37e3d6a28c
Kevin R. Dufendach, MD, MS <kevin.dufendach@cchmc.org>
2024-05-05 16:22:41 -0400

    change locationEncounters to a single array of LocationEncounter data

--- src/ctx-location.ts
+++ src/ctx-location.ts
@@ -1,20 +1,36 @@
 import {CtxResource} from "./ctx-resource";
 import {CodeableConcept} from "fhir/r4";
 
-export type OperationalStatus = 'unoccupied' | 'occupied' | 'leaving' | 'arriving' | 'closed' | 'housekeeping' | 'reserved' | 'contaminated' | 'isolated'
+export type OperationalStatus =
+  'unoccupied'
+  | 'occupied'
+  | 'leaving'
+  | 'arriving'
+  | 'closed'
+  | 'housekeeping'
+  | 'reserved'
+  | 'contaminated'
+  | 'isolated'
+
+export type LocationStatus = 'planned' | 'active' | 'reserved' | 'completed';
+
+export interface LocationEncounter {
+  encounterId: string,
+  status: LocationStatus
+}
 
 export interface CtxLocation extends CtxResource {
-    resourceType: "CtxLocation"
-    name: string
-    identifier: string
-    physicalType: 'si' | 'bu' | 'wa' | 'ro' | 'bd' | 'ho'
+  resourceType: "CtxLocation"
+  name: string
+  identifier: string
+  physicalType: 'si' | 'bu' | 'wa' | 'ro' | 'bd' | 'ho'
 
-    operationalStatus: OperationalStatus
-    // operationalStatusText?: string
+  // operationalStatus: OperationalStatus
+  // operationalStatusText?: string
 
-    activeEncounterId?: string | null
-    futureEncounterId?: string[]
+  // activeEncounterId?: string | null
+  locationEncounters?: LocationEncounter[]
 
-    partOf?: string | null // ID of another location this is physically a part of
-    characteristics?: CodeableConcept[]
+  partOf?: string | null // ID of another location this is physically a part of
+  characteristics?: CodeableConcept[]
 }
