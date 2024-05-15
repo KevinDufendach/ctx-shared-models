01f4133de73d2453c803c5c3133c10fe18c44d00
Kevin R. Dufendach, MD, MS <kevin.dufendach@cchmc.org>
2024-05-15 15:28:48 -0400

    create LocationStatusMap

--- src/room-assignment.ts
+++ src/room-assignment.ts
@@ -1,7 +1,15 @@
 import {CtxResource} from "./ctx-resource";
 import {CtxPeriod} from "./shared/ctx-period";
+import {OperationalStatus} from "./ctx-location";
 
 export type LocationStatus = 'planned' | 'active' | 'reserved' | 'completed';
+
+export const LocationStatusMap: ({[key in LocationStatus]: OperationalStatus}) = {
+  planned: 'arriving',
+  active: 'occupied',
+  reserved: 'reserved',
+  completed: 'unoccupied'
+}
 
 export interface CtxRoomAssignment extends CtxResource {
   resourceType: 'room-assignment'
