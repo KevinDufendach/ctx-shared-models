90fda8138f2364c3c2fbd04eb3c92201fe8b25c4
Kevin R. Dufendach, MD, MS <kevin.dufendach@cchmc.org>
2024-06-01 18:37:11 -0400

    add RGB interface to CtxCareTeam.ts

--- src/ctx-care-team.ts
+++ src/ctx-care-team.ts
@@ -1,16 +1,18 @@
 import {CtxResource} from "./ctx-resource";
 import {CtxPeriod} from "./shared/ctx-period";
 
+export interface RGB {
+  r: number
+  g: number
+  b: number
+}
+
 export interface CtxCareTeam extends CtxResource {
   resourceType: "care-team",
   identifier: string, // Mandatory // External Ids for this item
   name: string, // Name of the team, such as crisis assessment team
 
-  color?: { // Color used to identify the team
-    r: number
-    g: number
-    b: number
-  }
+  color?: RGB
 
   participants: { // Members of the team
     practitionerId?: string // Reference to a practitioner ID if known
